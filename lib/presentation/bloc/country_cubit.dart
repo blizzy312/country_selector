d9e8ff5ce604107f146544139d6267fb6a1363fa
Temirlan Tashbolotov <phblizzy@gmail.com>
2025-08-19 19:26:29 +0600

    add core logic

---
+++ lib/presentation/bloc/country_cubit.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:country_selector/data/repositories/country_repository.dart';
+import 'package:country_selector/models/country.dart';
+import 'package:country_selector/models/state.dart';
+import 'package:country_selector/presentation/bloc/country_state.dart';
+
+class CountryCubit extends Cubit<CountrySelectorState> {
+  final CountryRepository repository;
+
+  CountryCubit({required this.repository}) : super(const CountrySelectorState());
+
+  Future<void> loadCountries() async {
+    emit(state.copyWith(isLoadingCountries: true, clearCountriesError: true));
+
+    try {
+      final countries = await repository.getCountries();
+      emit(state.copyWith(
+        countries: countries,
+        isLoadingCountries: false,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        isLoadingCountries: false,
+        countriesError: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> selectCountry(Country country) async {
+    emit(state.copyWith(
+      selectedCountry: country,
+      clearSelectedState: true,
+      states: [],
+      clearStatesError: true,
+    ));
+
+    await loadStates(country.id);
+  }
+
+  Future<void> loadStates(int countryId) async {
+    emit(state.copyWith(isLoadingStates: true, clearStatesError: true));
+
+    try {
+      final states = await repository.getStatesByCountryId(countryId);
+      emit(state.copyWith(
+        states: states,
+        isLoadingStates: false,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        isLoadingStates: false,
+        statesError: e.toString(),
+      ));
+    }
+  }
+
+  void selectState(State selectedState) {
+    emit(state.copyWith(selectedState: selectedState));
+  }
+
+  Future<void> retryCountries() async {
+    await loadCountries();
+  }
+
+  Future<void> retryStates() async {
+    if (state.selectedCountry != null) {
+      await loadStates(state.selectedCountry!.id);
+    }
+  }
+}
